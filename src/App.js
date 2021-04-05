08b6a5effab494c5c4cb29602ea9270988289b09
itshmnt <hemantxs5@gmail.com>
2021-04-05 20:10:09 +0530

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <h1>Hi! I'm a React App!</h1>
+        <p>I'm working.</p>
+        <Person name="Max"  age="29"/>
+        <Person name="Maxi"  age="35"/>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
