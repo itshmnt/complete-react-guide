a4bce1ca24c8fff62c9a96233bbcb1bdcc3cd335
itshmnt <hemantxs5@gmail.com>
2021-04-20 20:12:51 +0530

    Context API, contextType & useContext()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
-const cockpit = props => {
+const Cockpit = props => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
 
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
@@ -46,8 +50,9 @@ const cockpit = props => {
       <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 };
 
-export default React.memo(cockpit);
+export default React.memo(Cockpit);
